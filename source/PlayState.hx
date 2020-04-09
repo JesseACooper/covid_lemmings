fdaa8f712d1d8d2f64f2c57ac01e34ef19b47e6c
Jesse Cooper <jesse.cooper@joinroot.com>
2020-04-09 18:09:02 -0500

    Add ogmo tilemap

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,25 @@
 package;
 
+import flixel.FlxObject;
+import flixel.tile.FlxTilemap;
+import flixel.addons.editors.ogmo.FlxOgmo3Loader;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var mapTiles:FlxOgmo3Loader;
+	var map:FlxTilemap;
+
 	override public function create()
 	{
+		mapTiles = new FlxOgmo3Loader(AssetPaths.covid_lemmings__ogmo, AssetPaths.map_001__json);
+		map = mapTiles.loadTilemap(AssetPaths.tiles__png, "tiles");
+		map.follow();
+		map.setTileProperties(1, FlxObject.NONE);
+		map.setTileProperties(2, FlxObject.ANY);
+		map.setTileProperties(3, FlxObject.NONE);
+		map.setTileProperties(4, FlxObject.ANY);
+		add(map);
 		super.create();
 	}
 
